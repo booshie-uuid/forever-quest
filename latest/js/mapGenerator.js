c0721a5b64b73fe4087679ed961a31da7c3bf57f
booshie.uuid <matt@matthewlynch.net>
2024-08-09 15:18:40 +1000

    fixed inconsistent indenting in mapGenerator

--- latest/js/mapGenerator.js
+++ latest/js/mapGenerator.js
@@ -7,24 +7,24 @@ class MapGenerator
     }
 
     initializeGrid()
-	{
-		this.map.grid = this.map.grid || [];
+    {
+        this.map.grid = this.map.grid || [];
 
         for (let gridY = 0; gridY < this.map.gridRows; gridY++)
-		{
-			this.map.grid[gridY] = this.map.grid[gridY] || [];
+        {
+            this.map.grid[gridY] = this.map.grid[gridY] || [];
 
             for (let gridX = 0; gridX < this.map.gridCols; gridX++)
-			{
+            {
                 this.map.grid[gridY][gridX] = Room.generateEmptyRoom(gridX, gridY, 0).compress();
             }
         }
     }
 
-	generateRooms()
-	{
-		this.initializeGrid();
-		
+    generateRooms()
+    {
+        this.initializeGrid();
+        
         // keep track of the rooms that have been populated
         const rooms = [];
 
@@ -46,27 +46,27 @@ class MapGenerator
         deadends = deadends.filter(room => room.type === Room.TYPES.DEADEND);
 
         // choose a deadend at random to serve as the spawn room
-		const spawnRoom = this.chance.pick(deadends);
+        const spawnRoom = this.chance.pick(deadends);
 
-		spawnRoom.type = Room.TYPES.SPAWN;
-		this.map.updateRoom(spawnRoom);
+        spawnRoom.type = Room.TYPES.SPAWN;
+        this.map.updateRoom(spawnRoom);
 
         this.map.currentX = spawnRoom.x;
         this.map.currentY = spawnRoom.y;
 
-		// sort deadends by their distance from the spawn room
+        // sort deadends by their distance from the spawn room
         // this will let us place rarer events further away from spawn
-		deadends.sort((roomA, roomB) => {
-			const distanceA = Math.abs(roomA.x - spawnRoom.x) + Math.abs(roomA.y - spawnRoom.y);
-			const distanceB = Math.abs(roomB.x - spawnRoom.x) + Math.abs(roomB.y - spawnRoom.y);
-			return distanceB - distanceA;       
+        deadends.sort((roomA, roomB) => {
+            const distanceA = Math.abs(roomA.x - spawnRoom.x) + Math.abs(roomA.y - spawnRoom.y);
+            const distanceB = Math.abs(roomB.x - spawnRoom.x) + Math.abs(roomB.y - spawnRoom.y);
+            return distanceB - distanceA;       
         });
 
         // place the exit room at the farthest deadend from the spawn room
         const exitRoom = deadends.shift();
 
-		exitRoom.type = Feature.TYPES.EXIT;
-		this.map.updateRoom(exitRoom);
+        exitRoom.type = Feature.TYPES.EXIT;
+        this.map.updateRoom(exitRoom);
 
         this.map.exploreRoom(exitRoom.x, exitRoom.y);
 
@@ -126,111 +126,111 @@ class MapGenerator
         }
 
         // explore the spawn room
-		this.map.exploreRoom(spawnRoom.x, spawnRoom.y);
+        this.map.exploreRoom(spawnRoom.x, spawnRoom.y);
 
         // explore all the rooms if debug mode is enabled
-		if(this.map.debug)
-		{
-			for(let y = 0; y < this.map.gridRows - 1; y++)
-			{
-				for(let x = 0; x < this.map.gridCols - 1; x++)
-				{
-					this.map.exploreRoom(x, y);
-				}
-			}
-		}
+        if(this.map.debug)
+        {
+            for(let y = 0; y < this.map.gridRows - 1; y++)
+            {
+                for(let x = 0; x < this.map.gridCols - 1; x++)
+                {
+                    this.map.exploreRoom(x, y);
+                }
+            }
+        }
 
         // set the map state to ready
-		this.map.state = Map.STATES.MAP_READY;
-	}
+        this.map.state = Map.STATES.MAP_READY;
+    }
 
     generateFoundation(rooms)
     {
         // choose a random room towards the top left of the map to start the foundation
-		const startX = this.chance.roll(0, 4);
-		const startY = this.chance.roll(0, 4);
-		const startRoom = this.map.getRoom(startX, startY);
+        const startX = this.chance.roll(0, 4);
+        const startY = this.chance.roll(0, 4);
+        const startRoom = this.map.getRoom(startX, startY);
 
         // choose a random room towards the bottom right of the map to finish the foundation
-		const finishX = this.map.gridCols - 1 - this.chance.roll(0, 4);
-		const finishY = this.map.gridRows - 1 - this.chance.roll(0, 4);
-		const finishRoom = this.map.getRoom(finishX, finishY);
+        const finishX = this.map.gridCols - 1 - this.chance.roll(0, 4);
+        const finishY = this.map.gridRows - 1 - this.chance.roll(0, 4);
+        const finishRoom = this.map.getRoom(finishX, finishY);
         
         // generate a path between the start and finish rooms
         const foundationPath = this.generatePath(startRoom, finishRoom);
 
         // flag the start and end room of the foundation as deadends
         startRoom.type = Room.TYPES.DEADEND;
-		this.map.updateRoom(startRoom);
+        this.map.updateRoom(startRoom);
 
         finishRoom.type = Room.TYPES.DEADEND;
         this.map.updateRoom(finishRoom);
 
         // add all of the rooms along the foundation path to the list of populated rooms
-		rooms.push(...foundationPath);
+        rooms.push(...foundationPath);
     }
 
     generatePath(start, destination)
-	{
-		const path = [];
-		
-		let currentX = start.x;
-		let currentY = start.y;
+    {
+        const path = [];
+        
+        let currentX = start.x;
+        let currentY = start.y;
 
         // we will randomly pick a heading (horizontal or vertical) every few steps
         // to ensure the path we create is more interesting
-		let isMovingHorizontally = this.chance.flip();
-		let stepsSinceDirectionChange = 0;
-		
-		const move = (current, target) => (current < target ? current + 1 : current > target ? current - 1 : current);
+        let isMovingHorizontally = this.chance.flip();
+        let stepsSinceDirectionChange = 0;
+        
+        const move = (current, target) => (current < target ? current + 1 : current > target ? current - 1 : current);
 
-		while(currentX !== destination.x || currentY !== destination.y)
-		{
-			// move towards the destination
+        while(currentX !== destination.x || currentY !== destination.y)
+        {
+            // move towards the destination
             currentX = (isMovingHorizontally)? move(currentX, destination.x): currentX;
             currentY = (!isMovingHorizontally)? move(currentY, destination.y): currentY;
 
-			// force a switch in direction if we can no longer move closer in the current direction
+            // force a switch in direction if we can no longer move closer in the current direction
             isMovingHorizontally = (currentX === destination.x)? false: isMovingHorizontally;
             isMovingHorizontally = (currentY === destination.y)? true: isMovingHorizontally;
-	
-			path.push(this.map.getRoom(currentX, currentY));
+    
+            path.push(this.map.getRoom(currentX, currentY));
 
-			stepsSinceDirectionChange++;
-			const distance = (isMovingHorizontally)? Math.abs(destination.y - currentY): Math.abs(destination.x - currentX);
-			
-			if(stepsSinceDirectionChange > 3 && distance > 3)
-			{
-				isMovingHorizontally = this.chance.flip();
-				stepsSinceDirectionChange = 0;
-			}
-		}
+            stepsSinceDirectionChange++;
+            const distance = (isMovingHorizontally)? Math.abs(destination.y - currentY): Math.abs(destination.x - currentX);
+            
+            if(stepsSinceDirectionChange > 3 && distance > 3)
+            {
+                isMovingHorizontally = this.chance.flip();
+                stepsSinceDirectionChange = 0;
+            }
+        }
 
-		for (const room of path)
-		{
-			room.type = (room.type === Room.TYPES.EMPTY)? Room.TYPES.REGULAR: room.type;
-			this.map.updateRoom(room);
-		}
+        for (const room of path)
+        {
+            room.type = (room.type === Room.TYPES.EMPTY)? Room.TYPES.REGULAR: room.type;
+            this.map.updateRoom(room);
+        }
 
-		return path;
-	}
+        return path;
+    }
 
     generateCorridors(rooms)
     {
-		const targetCorridors = this.chance.range(50, 60);
+        const targetCorridors = this.chance.range(50, 60);
         let actualCorridors = 0;
-		let attempts = 0;
+        let attempts = 0;
 
-		while(actualCorridors < targetCorridors && attempts < 1000)
-		{
-			const startRoom = rooms[this.chance.roll(1, rooms.length) - 1];
+        while(actualCorridors < targetCorridors && attempts < 1000)
+        {
+            const startRoom = rooms[this.chance.roll(1, rooms.length) - 1];
             const direction = DIRECTIONS.getRandomDirection(this.chance);
-			const maxLength = this.chance.range(6, 16);
+            const maxLength = this.chance.range(6, 16);
 
-			const corridor = this.generateCorridor(startRoom, maxLength, direction, []);
+            const corridor = this.generateCorridor(startRoom, maxLength, direction, []);
 
-			if(corridor.length > 1)
-			{
+            if(corridor.length > 1)
+            {
                 const firstRoom = corridor.shift();
                 const lastRoom = corridor[corridor.length - 1];
 
@@ -242,54 +242,54 @@ class MapGenerator
                 // mark the first room in the cooridor as a regular room
                 // it may have previously been a deadend
                 firstRoom.type = Room.TYPES.REGULAR;
-				this.map.updateRoom(firstRoom);
+                this.map.updateRoom(firstRoom);
 
                 // mark the last room in the cooridor as a deadend
                 // deadends are important as they are where most encounters will be place
                 lastRoom.type = Room.TYPES.DEADEND;
-				this.map.updateRoom(lastRoom);
+                this.map.updateRoom(lastRoom);
 
                 // add all of the rooms in the corridor to the list of populated rooms
                 // (except the first room, which already existed)
                 rooms.push(...corridor);
-				
-				actualCorridors++;
-			}
+                
+                actualCorridors++;
+            }
 
-			attempts++;
-		}
+            attempts++;
+        }
     }
 
-	generateCorridor(room, maxLength, direction, corridor)
-	{
-    	let neighbors = [];
+    generateCorridor(room, maxLength, direction, corridor)
+    {
+        let neighbors = [];
 
-		switch(direction)
-		{
-			case DIRECTIONS.NORTH:
-				neighbors.push(this.map.getRoom(room.x, room.y - 1));
-				neighbors.push(this.map.getRoom(room.x + 1, room.y - 1));
-				neighbors.push(this.map.getRoom(room.x - 1, room.y - 1));
-				break;
+        switch(direction)
+        {
+            case DIRECTIONS.NORTH:
+                neighbors.push(this.map.getRoom(room.x, room.y - 1));
+                neighbors.push(this.map.getRoom(room.x + 1, room.y - 1));
+                neighbors.push(this.map.getRoom(room.x - 1, room.y - 1));
+                break;
 
-			case DIRECTIONS.EAST:
-				neighbors.push(this.map.getRoom(room.x + 1, room.y));
-				neighbors.push(this.map.getRoom(room.x + 1, room.y + 1));
-				neighbors.push(this.map.getRoom(room.x + 1, room.y - 1));
-				break;
+            case DIRECTIONS.EAST:
+                neighbors.push(this.map.getRoom(room.x + 1, room.y));
+                neighbors.push(this.map.getRoom(room.x + 1, room.y + 1));
+                neighbors.push(this.map.getRoom(room.x + 1, room.y - 1));
+                break;
 
-			case DIRECTIONS.SOUTH:
-				neighbors.push(this.map.getRoom(room.x, room.y + 1));
-				neighbors.push(this.map.getRoom(room.x + 1, room.y + 1));
-				neighbors.push(this.map.getRoom(room.x - 1, room.y + 1));
-				break;
+            case DIRECTIONS.SOUTH:
+                neighbors.push(this.map.getRoom(room.x, room.y + 1));
+                neighbors.push(this.map.getRoom(room.x + 1, room.y + 1));
+                neighbors.push(this.map.getRoom(room.x - 1, room.y + 1));
+                break;
 
-			case DIRECTIONS.WEST:
-				neighbors.push(this.map.getRoom(room.x - 1, room.y));
-				neighbors.push(this.map.getRoom(room.x - 1, room.y + 1));
-				neighbors.push(this.map.getRoom(room.x - 1, room.y - 1));
-				break;
-		}
+            case DIRECTIONS.WEST:
+                neighbors.push(this.map.getRoom(room.x - 1, room.y));
+                neighbors.push(this.map.getRoom(room.x - 1, room.y + 1));
+                neighbors.push(this.map.getRoom(room.x - 1, room.y - 1));
+                break;
+        }
 
         // remove any neighbors that are null (off the edge of the map)
         neighbors = neighbors.filter(neighbor => neighbor !== null);
@@ -300,13 +300,13 @@ class MapGenerator
         // if any of the neighbors are not empty, we have gone as far as we can go
         if(neighbors.some(neighbor => neighbor.type !== Room.TYPES.EMPTY)) { return corridor; }
 
-		if (room.type === Room.TYPES.EMPTY)
-		{
-			room.type = Room.TYPES.REGULAR;
-			this.map.updateRoom(room);
-		}
+        if (room.type === Room.TYPES.EMPTY)
+        {
+            room.type = Room.TYPES.REGULAR;
+            this.map.updateRoom(room);
+        }
 
-		corridor.push(room);
+        corridor.push(room);
 
         if(neighbors.length === 0)
         { 
@@ -324,7 +324,7 @@ class MapGenerator
             // continue trying to extend the corridor in the desired direction
             return this.generateCorridor(neighbors[0], maxLength, direction, corridor);
         }
-	}
+    }
 
     generateLoops(count, deadends)
     {
