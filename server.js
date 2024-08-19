4692b0cb9110e0dfc01746ff3c468ef0ba5e2e47
booshie.uuid <matt@matthewlynch.net>
2024-08-19 20:49:55 +1000

    fixing issue with port between local and server deployment

--- server.js
+++ server.js
@@ -27,6 +27,8 @@ io.on('connection', (socket) => {
 
 });
 
-server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log(`app listening on port ${port}`);
 });
